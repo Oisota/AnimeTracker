b00ff48880a9293462c402848b86dd781559c479
Derek Morey <dman6505@gmail.com>
2015-12-04 00:25:00 -0500

    Began Implementation
    The basic structure of the project was put in place and basic
    features have been implemented.

---
+++ popup.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function() {
+    /*
+    chrome.storage.sync.set({
+        shows: [{
+            title: 'Yu Yu Hakusho',
+            base_url: 'http://dubbed-anime.com/yu-yu-hakusho-episode-{}-dubbed',
+            url: 'http://dubbed-anime.com/yu-yu-hakusho-episode-1-dubbed',
+            episode: 1
+        },{
+            title: 'Dragon Ball Z',
+            base_url: 'http://dubbed-anime.com/yu-yu-hakusho-episode-{}-dubbed',
+            url: 'http://dubbed-anime.com/yu-yu-hakusho-episode-1-dubbed',
+            episode: 1
+        },{
+            title: 'Mobil Suit Gundam Wing',
+            base_url: 'http://dubbed-anime.com/yu-yu-hakusho-episode-{}-dubbed',
+            url: 'http://dubbed-anime.com/yu-yu-hakusho-episode-1-dubbed',
+            episode: 1
+        }]
+    });
+    */
+    chrome.storage.sync.get('shows', util.load);
+
+    document.getElementById('addshow').onclick = function() {
+        document.getElementById('addshowform').style.display = 'block';
+        document.getElementById('main').style.display = 'none';
+    };
+
+    document.getElementById('canceladd').onclick = function() {
+        document.getElementById('addshowform').style.display = 'none';
+        document.getElementById('main').style.display = 'block';
+    };
+
+    document.getElementById('addshowbtn').onclick = function() {
+        chrome.storage.sync.get('shows', function(s) {
+            s.shows.push(util.getShowInfo()); //append new show object to show array 
+            util.load({shows: [s.shows[s.shows.length -1]]}); //update show list
+            chrome.storage.sync.set(s); //save shows back to storage
+            document.getElementById('addshowform').style.display = 'none';
+            document.getElementById('main').style.display = 'block';
+            util.resetAddShowFields();
+        });
+    };
+
+    document.getElementById('delshows').onclick = function() {
+        chrome.storage.sync.remove('shows');
+    };
+});
+
