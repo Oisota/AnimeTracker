6427a5cf3fa270668f0056a34f262258c17e2086
Derek Morey <dman6505@gmail.com>
2015-12-10 15:28:58 -0500

    Made functions generic.
    The addShowFieldsEmpty, and resetAddShowFieds functions were
    removed and replaced with generic fieldsEmpty and resetFields
    functions. These new functions iterate through a list of field
    Id's to achieve functionality. This was done to increase the
    reusability of the functions.
    
    The Show function was also updated to get its data from any three
    field Id's. This was done so that the function can grab data from
    any input source (either the new show form or from a shows own
    info form).

--- js/util.js
+++ js/util.js
@@ -4,12 +4,12 @@
      * it to chrome storage
      */
     exports.addShow = function() {
-        if (addShowFormEmpty()) {
+        if (fieldsEmpty(['title-input','url-input','episode-input'])) {
             return;
         }
         chrome.storage.sync.get('shows', function(s) {
             var iframe = document.getElementById('sandbox-frame');
-            var show = Show();
+            var show = Show('title-input', 'url-input', 'episode-input');
             var message = {
                 command: 'render',
                 template: 'show',
@@ -23,7 +23,7 @@
             s.shows.push(show); //append new show object to show array 
             chrome.storage.sync.set(s); //save shows back to storage
             iframe.contentWindow.postMessage(message, '*'); 
-            resetAddShowFields();
+            resetFields(['title-input','url-input','episode-input']);
         });
     }
 
@@ -31,26 +31,25 @@
      * Load shows and post a message to the sandboxed
      * iframe.
      */
-    exports.loadShows = function(items) {
+    exports.loadShows = function(shows) {
         var iframe = document.getElementById('sandbox-frame');
         var message = {
             command: 'render',
             template: 'show',
-            context: items
+            context: shows
         };
         iframe.contentWindow.postMessage(message, '*');
     }
 
     /*
-     * Create new show object from the input data
-     * in the add show form
+     * Create new show object from the given input field Id's
      */
-    var Show = function() {
+    var Show = function(titleField, urlField, episodeField) {
         var show = {};
 
-        show.title = $('#title-input').val();
-        show.base_url = $('#url-input').val();
-        show.episode = $('#episode-input').val();
+        show.title = $('#' + titleField).val();
+        show.base_url = $('#' + urlField).val();
+        show.episode = $('#' + episodeField).val();
         show.url = show.base_url.replace('{}', show.episode);
         show.removeId = generateId();
         show.confirmRemoveId = generateId();
@@ -63,34 +62,34 @@
 
     /*
      * Update the given url with the given
-     * episode number
+     * episode number and return the new url
      */
     var updateUrl = function(episode, base_url) {
         return base_url.replace('{}', episode);
     }
 
     /*
-     * Returns true if any of the fields in the
-     * add show form are blank
+     * Check if given fields are empty. Returns
+     * true if any one field is empty.
      */
-    var addShowFormEmpty = function() {
-        var title = $('#title-input').val().trim();
-        var base_url = $('#url-input').val().trim();
-        var episode = $('#episode-input').val().trim();
-
-        if (title === '' || base_url === '' || episode === '') {
-            return true;
+    var fieldsEmpty = function(fields) {
+        for (var i=0; i<fields.length; i++) {
+            field = $('#' + fields[i]).val().trim();
+            if (field === '') {
+                return true;
+            }
         }
         return false;
     }
+        
 
     /*
-     * Set add show fields to be blank
+     * blank out the given array of field Id's
      */
-    var resetAddShowFields = function() {
-        $('#title-input').val('');
-        $('#url-input').val('');
-        $('#episode-input').val('');
+    var resetFields = function(fields) {
+        for (var i=0; i<fields.length; i++) {
+            $('#' + fields[i]).val('');
+        }
     }
 
     /*
