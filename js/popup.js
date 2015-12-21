3a8ee1068e6e466c42b0ebf2d7033ced84824343
Derek Morey <dman6505@gmail.com>
2015-12-20 21:50:16 -0500

    Removed page load event listener
    The event listener for the page load event was removed and the
    loading shows from chrome storage now happens inside the document
    ready listener. This was done because the page load listener was
    only there to make the sandboxed template work properly. Since
    templates are now precompiled, the lisenter is no longer necessary
    and loading the shows can happen as soon as the document is ready.

--- js/popup.js
+++ js/popup.js
@@ -2,13 +2,6 @@
  * Execute code when the document has loaded fully
  */
 $(document).ready(function() {
-    $('#add-show').click(Storage.addShow);
-});
-
-/*
- * When the window has fully loaded, load the
- * list of shows from chrome storage
- */
-window.addEventListener('load', function() {
     chrome.storage.sync.get('shows', Storage.loadShows);
+    $('#add-show').click(Storage.addShow);
 });
