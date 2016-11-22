f763b3d57795b08f46e186b519a198692673d83d
Derek Morey <dman6505@gmail.com>
2016-11-21 20:26:26 -0500

    Created view for the add show form
    A Backbone view was created for the add show form functionality.
    This was done to simplifiy the maintence of that functionality.

--- js/main.js
+++ js/main.js
@@ -14,37 +14,7 @@ shows.load({
     }
 });
 
-//event handler for adding a new show
-document
-.getElementById('add-show')
-.addEventListener('click', (event) => {
-    const fields = ['#title-input','#url-input','#episode-input'];
-    const fieldsEmpty = fields.reduce((prev, cur) => {
-        return $(cur).val().trim() === '' && prev;
-    }, true);
-    if (fieldsEmpty) {
-        return;
-    }
-    //get show data
-    const title = $('#title-input').val();
-    const baseUrl = $('#url-input').val();
-    const episode = $('#episode-input').val();
-    const url = baseUrl.replace('{}', episode);
-
-    shows.add({
-        title: title,
-        baseUrl: baseUrl,
-        url: url,
-        episode: episode
-    });
-
-    fields.forEach(field => $(field).val('')); //clear fields
-});
-
-//event handler for cancel add show button
-document
-.getElementById('cancel-add')
-.addEventListener('click', (event) => {
-    const fields = ['#title-input','#url-input','#episode-input'];
-    fields.forEach(field => $(field).val('')); //clear fields
+const addShowView = new App.views.AddShow({
+    el: document.getElementById('new-show-form'),
+    collection: shows
 });
---
+++ js/views/addshow.js
@@ -0,0 +1,43 @@
+'use strict';
+var App = App || {};
+App.views = App.views || {};
+
+App.views.AddShow = Backbone.View.extend({
+    fields: ['#title-input','#url-input','#episode-input'],
+    template: App.templates.renderAddShow,
+    events: {
+        'click #add-show': 'addShow',
+        'click #cancel-add': 'cancelAdd'
+    },
+    initialize: function() {
+        this.render();
+    },
+    render: function() {
+        const html = this.template();
+        this.$el.html(html);
+        return this;
+    },
+    addShow: function() {
+        const fieldsEmpty = this.fields.reduce((prev, cur) => ($(cur).val().trim() === '' && prev), true);
+        if (fieldsEmpty) {
+            return;
+        }
+        //get show data
+        const title = this.$('#title-input').val();
+        const baseUrl = this.$('#url-input').val();
+        const episode = this.$('#episode-input').val();
+        const url = baseUrl.replace('{}', episode);
+
+        this.collection.add({
+            title: title,
+            baseUrl: baseUrl,
+            url: url,
+            episode: episode
+        });
+    
+        this.fields.forEach(field => $(field).val('')); //clear fields
+    },
+    cancelAdd: function() {
+        this.fields.forEach(field => $(field).val('')); //clear fields
+    },
+});
